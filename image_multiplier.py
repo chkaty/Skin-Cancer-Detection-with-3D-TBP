e4eb39d2fcf6333a07aa6fc0cee01a8529ff79b1
Manny1008 <159737646+Manny1008@users.noreply.github.com>
2025-03-08 13:42:40 -0500

    Update image_multiplier.py

--- image_multiplier.py
+++ image_multiplier.py
@@ -4,11 +4,13 @@ import random
 
 def image_multiplier(img, n):
     variants = []
+    orig_width, orig_height = img.size
+    
     for i in range(n):
         tmp = img.copy()
         
         # random rotation
-        angle = random.uniform(-30, 30)
+        angle = random.choice([90, 180, 270])
         tmp = tmp.rotate(angle, expand = True)
         
         # random brightness, contrast, and color
@@ -23,16 +25,22 @@ def image_multiplier(img, n):
         if random.random() < 0.5:
             tmp = ImageOps.mirror(tmp)
             
-        # random shear
-        shear = random.uniform(-0.5, 0.5)
-        tmp = tmp.transform(tmp.size, Image.AFFINE, (1, shear, 0, 0, 1, 0))
+        # random shear 
+        #shear = random.uniform(-0.5, 0.5)
+        #tmp = tmp.transform(tmp.size, Image.AFFINE, (1, shear, 0, 0, 1, 0))
+        #variants.append(tmp)
+
+        # random zoom
+        scale = random.uniform(0.9, 1.1)
+        tmpsize = (int(tmp.width * scale), int(tmp.height * scale))
+        tmp = tmp.resize(tmpsize, Image.BICUBIC)
+
         variants.append(tmp)
 
     return variants
 
 from IPython.display import display
 
-# test with malignant image
 img = Image.open('ISIC_9996602.jpg')
 test = image_multiplier(img, 5)
 for i in range(5):
